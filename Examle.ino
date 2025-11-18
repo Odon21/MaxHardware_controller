a690da947b45d44cf60c85af89418b038a56cf59
Hack-21 <82946332+Odon21@users.noreply.github.com>
2025-11-18 15:56:53 +0200

    Example how to use it

--- Examle.ino
+++ Examle.ino
@@ -1,1 +1,18 @@
+#include "MaxHardware_controller.h"
+MaxHardware_controller m;
+#define botao 0
+void setup() {
+  m.begin();
+  m.tft.fillScreen(TFT_RED);
+  delay(300);
+  m.tft.fillScreen(TFT_GREEN);
+  delay(300);
+  m.tft.fillScreen(TFT_BLUE);
+  delay(300);
+  m.tft.fillScreen(TFT_BLACK);
+}
+void loop() {
+  m.mapTouch(true);
+  m.tft.setCursor(20, 80);
+}
 
